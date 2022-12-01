c41c3a575ffad95c1ac129ada2a2e694d65b0b28
Denys Kudriev <denys_kudriev@epam.com>
2022-12-01 08:57:45 +0200

    Add unit tests for StringSumKata

---
+++ Kata/StringSumKata.cs
@@ -0,0 +1,10 @@
+﻿namespace Kata
+{
+    public static class StringSumKata
+    {
+        public static string AddStringNaturalNumbers(string num1, string num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KataTests/StringSumKataTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Kata;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class StringSumKataTests
+    {
+        [Test]
+        public void ShouldNotExceptNullAsFirstArgument()
+        {
+            string num1 = null;
+            var num2 = "124";
+
+            var action = () => StringSumKata.AddStringNaturalNumbers(num1, num2);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ShouldNotExceptNullAsSecondArgument()
+        {
+            var num1 = "124";
+            string num2 = null;
+
+            var action = () => StringSumKata.AddStringNaturalNumbers(num1, num2);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfFirstArgumentIsNotANaturalNumber()
+        {
+            var num1 = "0";
+            var num2 = "1512";
+
+            var action = () => StringSumKata.AddStringNaturalNumbers(num1, num2);
+
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfSecondArgumentIsNotANaturalNumber()
+        {
+            var num1 = "1245";
+            var num2 = "-1";
+
+            var action = () => StringSumKata.AddStringNaturalNumbers(num1, num2);
+
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ShouldSumNumbersCorrectly()
+        {
+            var num1 = "1245";
+            var num2 = "7192";
+
+            var result = StringSumKata.AddStringNaturalNumbers(num1, num2);
+
+            result.Should().Be("9437");
+        }
+
+        [Test]
+        public void ShouldHandleVeryLargeNumbersCorrectly()
+        {
+            var num1 = "100000000000000000000000000000000000000000000000000006";
+            var num2 = "1100000000000000000000000000000000000000000000000000001";
+
+            var result = StringSumKata.AddStringNaturalNumbers(num1, num2);
+
+            result.Should().Be("1200000000000000000000000000000000000000000000000000007");
+        }
+    }
+}
