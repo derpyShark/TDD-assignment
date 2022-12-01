e496a47888404f6d827848ea56e770d37db66907
Denys Kudriev <denys_kudriev@epam.com>
2022-12-01 09:32:30 +0200

    Add tests for Calc stats kata

---
+++ Kata/CalcStatsKata.cs
@@ -0,0 +1,25 @@
+﻿namespace Kata
+{
+    public static class CalcStatsKata
+    {
+        public static int GetMinimumValue(IEnumerable<int> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetMaximumValue(IEnumerable<int> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetNumberOfElementsInTheSequence(IEnumerable<int> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static float GetAverageValue(IEnumerable<int> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KataTests/CalcStatsKataTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using Kata;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class CalcStatsKataTests
+    {
+        [Test]
+        public void GetMinimumValueShouldThrowExceptionIfSequenceIsEmpty()
+        {
+            var sequence = new int[0];
+
+            var action = () => CalcStatsKata.GetMinimumValue(sequence);
+
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void GetMaximumValueShouldThrowExceptionIfSequenceIsEmpty()
+        {
+            var sequence = new int[0];
+
+            var action = () => CalcStatsKata.GetMaximumValue(sequence);
+
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void GetAverageValueShouldThrowExceptionIfSequenceIsEmpty()
+        {
+            var sequence = new int[0];
+
+            var action = () => CalcStatsKata.GetAverageValue(sequence);
+
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void GetMinimumValueShouldReturnMinimumValue()
+        {
+            var sequence = new int[] { 6, 9, 15, -2, 92, 11 };
+
+            var result = CalcStatsKata.GetMinimumValue(sequence);
+
+            result.Should().Be(-2);
+
+        }
+
+        [Test]
+        public void GetMaximumValueShouldReturnMaximumValue()
+        {
+            var sequence = new int[] { 6, 9, 15, -2, 92, 11 };
+
+            var result = CalcStatsKata.GetMinimumValue(sequence);
+
+            result.Should().Be(-2);
+        }
+
+        [Test]
+        public void GetAverageValueShouldReturnAverageValue()
+        {
+            var sequence = new int[] { 6, 9, 15, -2, 92, 11 };
+
+            var result = CalcStatsKata.GetMinimumValue(sequence);
+
+            result.Should().Be(-2);
+        }
+
+        [Test]
+        public void GetNumberOfElementsInTheSequenceShouldReturnNumberOfElementsInTheSequence()
+        {
+            var sequence = new int[] { 6, 9, 15, -2, 92, 11 };
+
+            var result = CalcStatsKata.GetNumberOfElementsInTheSequence(sequence);
+
+            result.Should().Be(6);
+        }
+
+        [Test]
+        public void GetNumberOfElementsInTheSequenceShouldReturn0WhenEmpty()
+        {
+            var sequence = new int[0];
+
+            var result = CalcStatsKata.GetNumberOfElementsInTheSequence(sequence);
+
+            result.Should().Be(0);
+        }
+    }
+}
